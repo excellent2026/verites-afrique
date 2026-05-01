5c14e5790cb7caf834bcb412691bcaaf18fd4e3f
excellent2026 <vangujosaphat@gmail.com>
2026-05-02 00:51:36 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+﻿const CACHE_NAME = 'verites-afrique-case-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './case.png'
+];
+
+// Installation du Service Worker et mise en cache des fichiers
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Case du Savoir : Fichiers mis en cache');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Stratégie de réponse : Cache d'abord, puis Réseau
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
